d66d4cbe4f6a9840cea9abb79b000a13f3fea405
Jack Clayton <jackclayto@gmail.com>
2022-05-31 16:33:24 +0800

    Fix tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -4,45 +4,6 @@
 local winwidth = 25
 
 -- :h nvim-tree.lua
-vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_respect_buf_cwd = 1
-
-vim.g.nvim_tree_git_hl = 0
-vim.g.nvim_tree_highlight_opened_files = 0
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_create_in_closed_folder = 0
-
-vim.g.nvim_tree_icon_padding = vim.g.global_symbol_padding or ' '
-vim.g.nvim_tree_special_files = {}
-vim.g.nvim_tree_show_icons = {
-	git = 1, folders = 1, files = 1, folder_arrows = 0 }
-vim.g.nvim_tree_icons = {
-	default = '',
-	symlink = '',
-	git = {
-		unstaged = '+',
-		staged = '+',
-		unmerged = '',
-		renamed = '',
-		untracked = '?',
-		deleted = '',
-		ignored = '!'
-	},
-	folder = {
-		arrow_open = '',
-		arrow_closed = '',
-		default = '',
-		open = '',
-		empty = '',
-		empty_open = '',
-		symlink = '',
-		symlink_open = '',
-	}
-}
-
--- Custom actions
-
 local function get_current_directory(node)
 	local path = node.absolute_path
 	if node.nodes == nil or not node.open then
